f9211befde6d38728a37856825020d1dc241c71e
chiranjeevi-gorantla <chiranjeevigorantla@gmail.com>
2021-06-20 20:38:05 +0530

    Rank, Particles - comp_build

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React, {Component} from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
 import './App.css';
 
-
+const partcilesOptions = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#ffffff",
+        blur: 5
+      }
+    }
+  }
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
+         <Particles className='particles'
+            params={partcilesOptions} />
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
-           {/*
-            <FaceRecognition />*/}
+           {/* <FaceRecognition /> */}
       </div>
     );
   }
--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import './ImageLinkForm.css';
 
 const ImageLinkForm = () =>{
   return(
       <div>
         <p className='f3'>
-          {'This is going to detect faces in your pictures. Give it a try'}
+          {'This will detect faces in your pictures. Try it out...'}
         </p>
-        <div>
-          <input className='f5 pa1 w-70 center' type='tex' placeholder='Give the image link'/>
-          <button className='w-30 grow f4 link pv1 dib bg-light-yellow'>Detect</button>
+          <div className='center'>
+            <div className='form center pa4 br3 shadow-5'>
+            <input className='f5 pa1 w-70 center' type='tex' placeholder='Paste the image link here'/><div className='ma1'></div>
+            <button className='w-30 br3 grow f4 link pv1 dib btn-color pointer'>Detect</button>
+          </div>
         </div>
       </div>
   );
